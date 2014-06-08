a53f037cafddf0c5096322cfb5288a7bf5dde4b3
OpenShift guest <barnabas_tanko@livingpicture.org>
2014-06-08 10:26:56 -0400

    bugfix

--- base/base_cguard.cpp
+++ base/base_cguard.cpp
@@ -11,11 +11,10 @@ namespace BASE {
 
 /////////////////////////////////////////////////////////////////////////////
 CGuard::CGuard(IObject & tObject, IObject::operations operation, IObject::modes mode, T_BOOLEAN bInverse) :
+  m_Object(&tObject),
   m_operation(operation),
   m_mode(mode),
   m_Inverse(bInverse) {
-  m_Object = &tObject;
-      
   if (m_Object.IsValid() == true) {
     if (m_Inverse == false) {
       m_Object->Acquire(m_operation, m_mode);
