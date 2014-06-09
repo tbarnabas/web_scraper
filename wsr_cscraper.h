5ef6788bfde8ff0d3444d151123beac16e5083bf
OpenShift guest <barnabas_tanko@livingpicture.org>
2014-06-09 19:35:28 -0400

    regexp

--- wsr_cscraper.h
+++ wsr_cscraper.h
@@ -38,13 +38,6 @@ private:
   CScraper & operator=(const CScraper & tScraper);
 
 protected:
-  //! modular power (based on repeated sqaring on binary form algorithm)
-  T_ULONG Modular(T_ULONG uBase, T_ULONG uExponent, T_ULONG uDivisor);
-  //! match pattern (based on Rabin-Karp algorithm)
-  T_BOOLEAN RabinKarp(const T_BYTE * pBuffer, T_ULONG uBufferSize, const T_BYTE * pPattern, T_ULONG uPatternSize, T_ULONG & uIndex);
-  //! recognize string contents
-  REFERENCE< ::DATASTRUCTURE::CQueue<T_STRING> > RecognizeStringContents(const T_CHAR * pBuffer, T_ULONG uBufferSize, ::DATASTRUCTURE::CQueue<T_STRING> * pPatterns);
-
   //! receive data
   static size_t ReceiveData(void * pContents, size_t uSize, size_t uLength, void * pData);
   //! download home page
